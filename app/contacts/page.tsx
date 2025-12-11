fb349afd706222aab8636fdc474dc0526ffdbdd9
letacheru <letacheru484@gmail.com>
2025-12-11 10:57:19 +0300

    Add Partners section to Contact page and update background image URL for improved visual quality. Enhanced the Contact component by replacing the static map with an embedded Google Maps iframe for better interactivity and user experience.

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Breadcrump from '@/components/layouts/Breadcrump';
 import Contact from '@/components/pages/contacts/Contact';
+import Partners from '@/components/sections/Partners';
 const page = () => {
   return (
 <>
 <Breadcrump 
-  backgroundImage="https://images.unsplash.com/photo-1423666639041-f56000c27a9e?ixlib=rb-4.0.3&auto=format&fit=crop&w=2074&q=80"
+  backgroundImage="https://images.unsplash.com/photo-1423666639041-f56000c27a9e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80"
   title="Contact Us"
   subtitle="Get in touch with our team for expert advice and professional service"
 />
 <Contact />
+<Partners />
 </>
   )
 }
