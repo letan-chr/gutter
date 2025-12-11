18a206fca9122fb033933df952b06fb8be1ae7df
Leta Cheru <letacheru484@gmail.com>
2025-12-11 08:50:18 +0300

    Refactor Feature section layout and styling. Enhanced header design, updated feature card structure to a full-width diagonal layout, and improved text readability. Added response interceptors for axios instance in the API module to handle requests and responses more effectively. Updated package.json and package-lock.json to include axios dependency.

---
+++ api/Api.ts
@@ -0,0 +1,118 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_DOMAIN,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+const getBatchData = async (features: any) => {
+  const response = await axiosInstance.post("/batch", { features });
+  return response.data;
+};
+const postContactForm = async (data: any) => {
+  const response = await axiosInstance.post("/contact-message", data);
+  return response.data;
+};
+
+const getProducts = async () => {
+  const response = await axiosInstance.get("/ecommerce");
+  return response.data;
+};
+const getProductBySlug = async (id: string) => {
+  const response = await axiosInstance.get(`/products/${id}`);
+  return response.data;
+};
+const getProductCategories = async () => {
+  const response = await axiosInstance.get("/product-categories");
+  return response.data;
+};
+const getBlogsCategories = async () => {
+  const response = await axiosInstance.get("/blog-categories");
+  return response.data;
+};
+const getBlogs = async () => {
+  const response = await axiosInstance.get("/blogs");
+  return response.data;
+};
+const getBlogBySlug = async (id: string) => {
+  const response = await axiosInstance.get(`/blogs/${id}`);
+  return response.data;
+};
+const getAllTestimonials = async () => {
+  const response = await axiosInstance.get("/testimonials");
+  return response.data;
+};
+const getAllClients = async () => {
+  const response = await axiosInstance.get("/partners");
+  return response.data;
+};
+const getAllTeamMembers = async () => {
+  const response = await axiosInstance.get("/team-members");
+  return response.data;
+};
+const getAllAboutUsData = async () => {
+  const response = await axiosInstance.get("/contents");
+  return response.data;
+};
+const getAllContactUsData = async () => {
+  const response = await axiosInstance.get("/setup");
+  return response.data;
+};
+
+const postNewsletterSubscription = async (data: any) => {
+  const response = await axiosInstance.post("/newsletter-subscription", data);
+  return response.data;
+};
+const postProductInquiry = async (data: any) => {
+  const response = await axiosInstance.post("/enquiry", data);
+  return response.data;
+};
+const getAllAwards = async () => {
+  const response = await axiosInstance.get("/awards");
+  return response.data;
+};
+const getProjects = async () => {
+  const response = await axiosInstance.get("/projects");
+  return response.data;
+};
+const getSingleProject = async (id: string) => {
+  const response = await axiosInstance.get(`/projects/${id}`);
+  return response.data;
+};
+
+export {
+  getBatchData,
+  getProducts,
+  getProductBySlug,
+  getProductCategories,
+  getBlogsCategories,
+  getBlogs,
+  getBlogBySlug,
+  getAllTestimonials,
+  getAllClients,
+  getAllTeamMembers,
+  getAllAboutUsData,
+  getAllContactUsData,
+  postContactForm,
+  postNewsletterSubscription,
+  postProductInquiry,
+  getAllAwards,
+  getProjects,
+  getSingleProject,
+};
